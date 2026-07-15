97b5cd249b2bd4675f169075225f51ce6c3a88d0
abdulahzahoork <abdulahzahoork@gmail.com>
2026-07-15 17:25:49 +0500

    Practice Classes program 6

---
+++ C++ OOP (Summer Practice)/Classes/program_06.cpp
@@ -0,0 +1,41 @@
+// Problem 9: Calculator
+
+// Create overloaded functions named add().
+
+// Versions:
+
+// add(int, int)
+
+// add(double, double)
+
+// add(int, int, int)
+
+// Test all of them.
+
+#include <iostream>
+using namespace std;
+
+class Calculator{
+public: 
+    int add(int a, int b) {
+        return (a + b);
+    }
+
+    double add(double a, double b) {
+        return (a+b);
+    }
+
+    double add(int a, int b, int c) {
+        return a+b+c;
+    }
+};
+
+int main() {
+    Calculator c1;
+
+    cout << c1.add(1, 1) << endl;
+    cout << c1.add(1, 1, 1) << endl;
+    cout << c1.add(2.3, 1.7) << endl;
+
+    return 0;
+}
