e4ec431cf69a967b2801b58149ac11b309a81b23
Abdullah Khan <abdulahzahoork@gmail.com>
2026-09-09 12:07:20 +0500

    Practice Recursion program 2

---
+++ C++ Programming Fundamentals/C++ Recursion/program_02.cpp
@@ -0,0 +1,17 @@
+// Print numbers from 1 to n.
+
+#include <iostream>
+using namespace std;
+
+void print1toN (int n) {
+    if (n==0) {
+        return;
+    }
+
+    print1toN(n-1);
+    cout << n << " ";
+}
+
+int main() {
+    print1toN(5);
+}
